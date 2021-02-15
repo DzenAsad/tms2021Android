5c448b7efa70534c5665462f938289d6187acced
DzenAsad <notebook>
2021-02-15 19:21:15 +0300

    Create Lesson7

--- Lesson4/src/com/home/model/fabric/data/MyData.java
+++ Lesson4/src/com/home/model/fabric/data/MyData.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class MyData {
     int count = 0;
     List<Object> initData = new LinkedList<>();
+    MyData myLink;
 
+    MyData(){
+        myLink = this;
+    }
 
     public Object getNextData() {
         if(initData.isEmpty()) {
@@ -16,5 +20,10 @@ public class MyData {
             return initData.get(count++);
         }
         return null;
+    }
+
+    public MyData addNextData(Object object){
+        initData.add(object);
+        return myLink;
     }
 }
