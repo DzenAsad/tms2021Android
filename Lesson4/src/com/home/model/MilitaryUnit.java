9eecf06f77b778cd0eb6776a29b17c6bfe94f379
DzenAsad <notebook>
2021-02-21 11:40:09 +0300

    Lesson 7: Add index to sortListPerson()

--- Lesson4/src/com/home/model/MilitaryUnit.java
+++ Lesson4/src/com/home/model/MilitaryUnit.java
@@ -58,6 +58,10 @@ public class MilitaryUnit {
             tmpArrPerson[i] = personSet.get(i).getSurname() + " " + personSet.get(i).getName();
         }
         Arrays.sort(tmpArrPerson);
+        for (int i = 0; i < tmpArrPerson.length; i++) {
+
+            tmpArrPerson[i] = i + 1 + ". " + tmpArrPerson[i];
+        }
         System.out.println(Arrays.deepToString(tmpArrPerson));
     }
 }
