cb1418eb9147471f5e25eb7892edf92ba2312c0d
DzenAsad <notebook>
2021-02-21 11:29:07 +0300

    Lesson 7: Add MissingAddressException

--- Lesson4/src/com/home/exception/MyNPE.java
+++
@@ -1,9 +0,0 @@
-package com.home.exception;
-
-import java.io.IOException;
-
-public class MyNPE extends NullPointerException {
-    public  MyNPE(String string) {
-        System.err.println(string + " is Null");
-    }
-}
