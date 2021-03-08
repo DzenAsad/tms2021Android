2980bc512643952cd623a8e4eb265428aac106f5
DzenAsad <xetq@mail.ru>
2021-03-08 23:59:00 +0300

    MyShop: MenuHandlerBookkeeping fix

--- MyShop/src/com/examShop/UI/menu/menuHandler/MenuHandlerBookkeeping.java
+++ MyShop/src/com/examShop/UI/menu/menuHandler/MenuHandlerBookkeeping.java
@@ -4,7 +4,9 @@ import com.examShop.UI.menu.MenuCase;
 import com.examShop.model.product.Product;
 import com.examShop.model.shop.Shop;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MenuHandlerBookkeeping extends MenuHandler {
     public MenuHandlerBookkeeping() {
@@ -15,19 +17,24 @@ public class MenuHandlerBookkeeping extends MenuHandler {
     public MenuCase casesSwitch(String command) {
         switch (command) {
             case ("1"): {
-                showCountOfAllTypes(getShop());
+                System.out.println("Shop have " + getShop().getProductsTypeInShop().size() + " types");
                 return MenuCase.MENU_BOOKKEEPING;
             }
             case ("2"): {
-                showCountOfAllProducts(getShop());
+                System.out.println("Shop have " + getShop().getAllProductsInShop().size() + " products");
                 return MenuCase.MENU_BOOKKEEPING;
             }
             case ("3"): {
-                showAverageCostOfProducts(getShop());
+                List<Product> tmpList = getShop().getAllProductsInShop();
+                System.out.println("Average cost of all Products " + String.format("%(.2f", showAverageCostOfProducts(tmpList)));
                 return MenuCase.MENU_BOOKKEEPING;
             }
             case ("4"): {
-                showAverageCostOfProductsByType(getShop());
+                List<Product> tmpList = getShop().getAllProductsInShop();
+                for (String string: getShop().getProductsTypeInShop()) {
+                    System.out.println("Average price for " + string + " is " + String.format("%(.2f", showAverageCostOfProductsByType(tmpList, string)));
+                }
+
                 return MenuCase.MENU_BOOKKEEPING;
             }
             case ("0"): {
@@ -39,37 +46,24 @@ public class MenuHandlerBookkeeping extends MenuHandler {
         }
     }
 
-    private void showCountOfAllTypes(Shop shop) {
-        System.out.println("Shop have " + shop.getProductsTypeInShop().size() + " types");
-    }
-
-    private void showCountOfAllProducts(Shop shop) {
-        System.out.println("Shop have " + shop.getAllProductsInShop().size() + " products");
-    }
-
-    private void showAverageCostOfProducts(Shop shop) {
-        List<Product> tmpList = shop.getAllProductsInShop();
+    private double showAverageCostOfProducts(List<Product> tmpList) {
         double tmp = 0;
         for (Product product : tmpList) {
             tmp += product.getPrice();
         }
-        tmp = tmp / tmpList.size();
-
-        System.out.println("Average cost of all Products " + String.format("%(.2f", tmp));
+        return tmp / tmpList.size();
     }
 
-    private void showAverageCostOfProductsByType(Shop shop) {
-        for (String type : shop.getProductsTypeInShop()) {
+    private double showAverageCostOfProductsByType(List<Product> tmpList, String type) {
             double allSum = 0;
             int countProducts = 0;
-            for (Product product : shop.getAllProductsInShop()) {
+            for (Product product : tmpList) {
                 if (type.equals(product.getType())) {
                     allSum += product.getPrice();
                     countProducts++;
                 }
             }
-            System.out.println("Average price for " + type + " is " + String.format("%(.2f", allSum / countProducts));
-        }
+            return allSum / countProducts;
     }
 }
 
